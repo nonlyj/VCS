88542a62d1b814a46e1455646e04dfe5cab95478
nonlyj <nonlyj@qq.com>
2026-03-12 20:50:29 +0800

    [VCS][LVGL]

---
+++ lv_port_linux/main.c
@@ -0,0 +1,197 @@
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+
+#include "lv_port_demos/lv_port_demos.h"
+
+#include "lv_port_indev.h"
+
+#if LV_USE_WAYLAND
+#include "backends/interface.h"
+#endif
+
+uint16_t window_width;
+uint16_t window_height;
+bool fullscreen;
+bool maximize;
+
+pthread_mutex_t lvgl_mutex; // 全局互斥锁
+
+static void configure_simulator(int argc, char **argv);
+
+static const char *getenv_default(const char *name, const char *dflt)
+{
+    return getenv(name) ? : dflt;
+}
+
+#if LV_USE_EVDEV
+static void lv_linux_init_input_pointer(lv_display_t *disp)
+{
+    /* Enables a pointer (touchscreen/mouse) input device
+     * Use 'evtest' to find the correct input device. /dev/input/by-id/ is recommended if possible
+     * Use /dev/input/by-id/my-mouse-or-touchscreen or /dev/input/eventX
+     */
+    //const char *input_device = getenv("LV_LINUX_EVDEV_POINTER_DEVICE");
+    const char *input_device = getenv_default("LV_LINUX_EVDEV_POINTER_DEVICE", "/dev/input/event1");
+
+    if (input_device == NULL) {
+        fprintf(stderr, "please set the LV_LINUX_EVDEV_POINTER_DEVICE environment variable\n");
+        printf("please set the LV_LINUX_EVDEV_POINTER_DEVICE environment variable\n");
+        exit(1);
+    }
+
+    lv_indev_t *touch = lv_evdev_create(LV_INDEV_TYPE_POINTER, input_device);
+    lv_indev_set_display(touch, disp);
+
+    /* Set the cursor icon */
+    LV_IMAGE_DECLARE(mouse_cursor_icon);
+    lv_obj_t * cursor_obj = lv_image_create(lv_screen_active());
+    lv_image_set_src(cursor_obj, &mouse_cursor_icon);
+    lv_indev_set_cursor(touch, cursor_obj);
+}
+#endif
+
+#if LV_USE_LINUX_FBDEV
+static void lv_linux_disp_init(void)
+{
+    const char *device = getenv_default("LV_LINUX_FBDEV_DEVICE", "/dev/fb0");
+    lv_display_t * disp = lv_linux_fbdev_create();
+
+#if LV_USE_EVDEV
+    lv_linux_init_input_pointer(disp);
+#endif
+
+    lv_linux_fbdev_set_file(disp, device);
+}
+#elif LV_USE_LINUX_DRM
+static void lv_linux_disp_init(void)
+{
+    const char *device = getenv_default("LV_LINUX_DRM_CARD", "/dev/dri/card0");
+    lv_display_t * disp = lv_linux_drm_create();
+
+#if LV_USE_EVDEV
+    lv_linux_init_input_pointer(disp);
+#endif
+
+    lv_linux_drm_set_file(disp, device, -1);
+}
+#elif LV_USE_SDL
+static void lv_linux_disp_init(void)
+{
+
+    lv_sdl_window_create(window_width, window_height);
+
+}
+#elif LV_USE_WAYLAND
+    /* see backend/wayland.c */
+#else
+#error Unsupported configuration
+#endif
+
+#if LV_USE_WAYLAND == 0
+void lv_linux_run_loop(void)
+{
+    uint32_t idle_time;
+
+    /*Handle LVGL tasks*/
+    while(1) {
+
+        pthread_mutex_lock(&lvgl_mutex);
+
+        idle_time = lv_timer_handler(); /*Returns the time to the next timer execution*/
+        pthread_mutex_unlock(&lvgl_mutex);
+        usleep(idle_time * 1000);
+    }
+}
+#endif
+
+/*
+ * Process command line arguments and environment
+ * variables to configure the simulator
+ */
+static void configure_simulator(int argc, char **argv)
+{
+
+    int opt = 0;
+    bool err = false;
+
+    /* Default values */
+    fullscreen = maximize = false;
+    window_width = atoi(getenv("LV_SIM_WINDOW_WIDTH") ? : "800");
+    window_height = atoi(getenv("LV_SIM_WINDOW_HEIGHT") ? : "480");
+
+    /* Parse the command-line options. */
+    while ((opt = getopt (argc, argv, "fmw:h:")) != -1) {
+        switch (opt) {
+        case 'f':
+            fullscreen = true;
+            if (LV_USE_WAYLAND == 0) {
+                fprintf(stderr, "The SDL driver doesn't support fullscreen mode on start\n");
+                exit(1);
+            }
+            break;
+        case 'm':
+            maximize = true;
+            if (LV_USE_WAYLAND == 0) {
+                fprintf(stderr, "The SDL driver doesn't support maximized mode on start\n");
+                exit(1);
+            }
+            break;
+        case 'w':
+            window_width = atoi(optarg);
+            break;
+        case 'h':
+            window_height = atoi(optarg);
+            break;
+        case ':':
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+            exit(1);
+        case '?':
+            fprintf (stderr, "Unknown option -%c.\n", optopt);
+            exit(1);
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+
+    configure_simulator(argc, argv);
+
+    /* Initialize mutex */
+    pthread_mutex_init(&lvgl_mutex, NULL);
+
+    /* Initialize LVGL. */
+    lv_init();
+
+    /* Initialize the configured backend SDL2, FBDEV, libDRM or wayland */
+    lv_linux_disp_init();
+
+    /* MY CODE */
+    //my_test();
+    //mfd();
+    vcs();
+    //my_mqtt_led();
+
+    //lv_port_indev_init();
+
+    /*Create a Demo*/
+    //lv_demo_widgets();
+    //lv_demo_widgets_start_slideshow();
+
+    //lv_demo_benchmark();
+    //lv_demo_stress();
+
+    //LV_LOG_USER("LV_LOG Test");
+    //LV_LOG("LV_LOG Test\n");
+    //LV_LOG("STAGING_DIR: %s\n", getenv("STAGING_DIR"));
+
+    lv_linux_run_loop();
+
+    return 0;
+}
